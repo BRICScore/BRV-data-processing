80b104aa6dfa04466649e6bedaf5f20242954c94
Qzmik <miko.kuzmicz@gmail.com>
2026-03-19 19:30:44 +0100

    Add support for revised labels and fix issues with malformed requests

--- database_access/requests.py
+++ database_access/requests.py
@@ -11,7 +11,6 @@ def uploadMeasurement(filepath_raw: str, filepath_work: str):
     print("Fill measurement metadata:")
     form_data["person_id"] = str(input("Person ID (currently email): "))
 
-    
 
     workMeasurementFileHook = open(filepath_work, "rb")
     jsonlines = workMeasurementFileHook.readlines()
@@ -20,12 +19,26 @@ def uploadMeasurement(filepath_raw: str, filepath_work: str):
     form_data["duration_ms"] = parsed_line["timestamp"]
     form_data["timestamp"] = time.time()
 
-    print('Input labels for the measurement - FORMAT: "label1", "label2", ...')
-    labels = str(input())
-    labels = f"[{labels}]"
-    form_data["labels"] = labels
+    print("Fill in labels for the measurement:")
 
-    files = {"measurement_file_raw" : open(filepath_raw, "rb"), "measurement_file_work": open(filepath_work, "rb")}
+    labels = {}
+
+    level = input("Level (raw/clean): ")
+    labels["level"] = level
+
+    activity = input("Activity: ")
+    labels["activity"] = activity
+
+    age = int(input("Age (from 0 to 100): "))
+    gender = input("Biological gender (male/female): ")
+    health = input("Health condition: ")
+    condition = input("Athletic condition: ")
+
+    labels["bio"] = {"age": age, "gender": gender, "health": health, "condition": condition}
+
+    form_data["labels"] = json.dumps(labels)
+    
+    files = {"measurement_file_raw" : open(filepath_raw, "rb"), "measurement_file_clean": open(filepath_work, "rb")}
 
     r = requests.put('https://brics-api.electimore.xyz/measurement/upload', headers=headers, files=files, data=form_data)
 
@@ -35,28 +48,35 @@ def uploadMeasurement(filepath_raw: str, filepath_work: str):
     
 def downloadMeasurement():
     headers = {"CF-Access-Client-Id": os.getenv("ACCESS_CLIENT_ID"), "CF-Access-Client-Secret": os.getenv("ACCESS_CLIENT_SECRET")}
-    query_data = {}
     #TODO: MOVE UI TO AN APP/WEBPAGE
     print("Fill measurement download query parameters")
-    person_id = str(input("Person ID (currently email): "))
-    if person_id != "":
-        query_data["person_id"] = person_id
-    print('Input labels for the query - FORMAT: "label1", "label2", ...')
-    labels = f"[{str(input("Labels: "))}]"
-    if labels != "[]":
-        query_data["labels"] = labels
-    length_min = input("Minimum length in minutes: ")
-    if length_min == "":
-        length_min = 0
-        query_data["length_min"] = length_min * 60000 # duration in DB kept in milliseconds
-    length_max = input("Maximum length in minutes: ")
-    if length_max == "":
-        length_max = 24*60 # 1 day
-    if length_max != None:
-        query_data["length_max"] = length_max * 60000 
-    quality = str(input("Quality - work or raw: "))
-    if quality != "":
-        query_data["quality"] = quality
+
+    person_id = input("Person ID (currently email): ").strip() or None
+    
+    length_min = int(input("Minimum length in minutes: ").strip() or 0)
+    length_max = int(input("Maximum length in minutes: ").strip() or 24*60) # default value - 1 day
+    
+    age_min = int(input("Minimum age of subject: ").strip() or 0)
+    age_max = int(input("Maximum age of subject: ").strip() or 100)
+    
+    level = input("Enter acceptable levels (raw/clean) seperated by space or press enter for all").strip().split() or None
+    gender = input("Enter genders (male/female) seperated by space or skip for all").strip().split() or None
+    activity = input("Enter activities seperated by space or skip or all").strip().split() or None
+    condition = input("Enter conditions seperated by space or skip or all").strip().split() or None
+    health = input("Enter health statuses seperated by space or skip or all").strip().split() or None
+
+    query_data = {
+        "person_id": person_id,
+        "length_min": length_min * 60000, #length kept in db in milliseconds
+        "length_max": length_max * 60000,
+        "age_min": age_min,
+        "age_max": age_max,
+        "level": level,
+        "gender": gender,
+        "activity": activity,
+        "condition": condition,
+        "health": health
+    }    
     
     r = requests.get('https://brics-api.electimore.xyz/measurement/download', headers=headers, params=query_data)
 
@@ -76,7 +96,7 @@ def deleteMeasurement():
     measurement_id = input("Fill id of measurement to delete: ")
 
     query_data["measurement_id"] = measurement_id 
-    r = requests.get('https://brics-api.electimore.xyz/measurement/download', headers=headers, params=query_data)
+    r = requests.delete('https://brics-api.electimore.xyz/measurement/delete', headers=headers, params=query_data)
 
     print(r.status_code)
     print(r.text)
