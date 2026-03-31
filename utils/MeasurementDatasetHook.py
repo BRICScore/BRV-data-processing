69a717986347a3d81b0df5973111e49cb53f3c97
Qzmik <miko.kuzmicz@gmail.com>
2026-03-31 19:55:50 +0200

    WIP: Created base of handling the measurement zip

--- utils/MeasurementDatasetHook.py
+++ utils/MeasurementDatasetHook.py
@@ -1,22 +1,21 @@
 from pathlib import Path
+from typing import Literal
 import database_access.requests as db
-import shutil
-import tempfile
 import os
 from config import MEASUREMENT_ZIP_PATH
+import MeasurementDirectoryProvider as mdp
 
 class MeasurementDatasetHook:
     
-    def __init__(self):
+    def __init__(self, target: mdp.MeasurementType):
         try:
-            self.folder_path = Path(tempfile.mkdtemp())
-            shutil.unpack_archive(MEASUREMENT_ZIP_PATH, self.folder_path)
-            self.folder_path = self.folder_path / "measurements_dataset"
+            self.provider = mdp.MeasurementDirectoryProvider()
+            self.folder_path = self.provider.provide_directory(target)
             self.number_of_files = len([name for name in os.listdir(self.folder_path) if os.path.isfile(name)])  
             self.index = 0
             
         except Exception:
-            raise Exception("Missing a measurement zip directory")
+            raise Exception("Missing a measurement zip directory or target invalid")
         
     def __iter__(self):
         return self
